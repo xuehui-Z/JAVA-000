351781f089a940c850d60560852f3121b84fe882
xuehui <aixiaohui@126.com>
2020-11-04 00:08:14 +0800

    homework

---
+++ Week_03/nio02/src/main/java/io/github/kimmking/gateway/router/impl/MyHttpEndpointRouter.java
@@ -0,0 +1,12 @@
+package io.github.kimmking.gateway.router.impl;
+
+import io.github.kimmking.gateway.router.HttpEndpointRouter;
+
+import java.util.List;
+
+public class MyHttpEndpointRouter implements HttpEndpointRouter {
+    @Override
+    public String route(List<String> endpoints) {
+        return null;
+    }
+}
