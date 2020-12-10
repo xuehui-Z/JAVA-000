014e3e4fe1edbc4e8b1d09976cba39d674f7da91
July <aixiaohui@126.com>
2020-12-10 23:07:51 +0800

    ShardingSphere 简单XA事务实现

---
+++ Week_08/shardingSphere-automikos-xa/src/main/java/TransactionXaDemo.java
@@ -0,0 +1,80 @@
+import org.apache.shardingsphere.driver.api.yaml.YamlShardingSphereDataSourceFactory;
+import org.apache.shardingsphere.transaction.core.TransactionType;
+import org.apache.shardingsphere.transaction.core.TransactionTypeHolder;
+
+import javax.sql.DataSource;
+import java.io.File;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class TransactionXaDemo {
+    public static void main(String[] args) throws IOException, SQLException {
+        DataSource dataSource = getShardingDatasource();
+        cleanupData(dataSource);
+
+        TransactionTypeHolder.set(TransactionType.XA);
+
+        Connection conn = dataSource.getConnection();
+        String sql = "insert into t_order (u_id, o_id) VALUES (?, ?);";
+
+        System.out.println("First XA Start insert data");
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            conn.setAutoCommit(false);
+            for (int i = 1; i < 11; i++) {
+                statement.setLong(1, i);
+                statement.setLong(2, (int)(Math.random()*100));
+                statement.executeUpdate();
+            }
+//            conn.commit();
+        }
+
+        System.out.println("First XA insert successful");
+
+
+        System.out.println("Second XA Start insert data");
+        try (PreparedStatement statement = conn.prepareStatement(sql)) {
+            conn.setAutoCommit(false);
+            for (int i = 1; i < 11; i++) {
+                statement.setLong(1, i+10);
+                statement.setLong(2, (int)(Math.random()*100));
+                statement.executeUpdate();
+                // 抛出异常，第二个事务执行不成功，所有数据都没有保存。
+                // 不抛异常，事务正常提交，所有数据保存成功。
+                if (i == 6) throw new Exception();
+            }
+            conn.commit();
+        } catch (Exception e) {
+            System.out.println("Second XA insert failed");
+            conn.rollback();
+        } finally {
+            conn.close();
+        }
+        System.out.println("Second XA insert successful");
+    }
+
+    private static void cleanupData(DataSource dataSource) {
+        System.out.println("Delete all Data");
+        try (Connection conn = dataSource.getConnection(); Statement statement = conn.createStatement()) {
+            statement.execute("delete from t_order;");
+            conn.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Delete all Data successful");
+    }
+
+    /**
+     * 生成DataSource，文件路径自行替换
+     * @return
+     * @throws IOException
+     * @throws SQLException
+     */
+    static private DataSource getShardingDatasource() throws IOException, SQLException {
+        String fileName = "sharding-config.yml";
+        File yamlFile = new File(TransactionXaDemo.class.getClassLoader().getResource(fileName).getPath());
+        return YamlShardingSphereDataSourceFactory.createDataSource(yamlFile);
+    }
+}
