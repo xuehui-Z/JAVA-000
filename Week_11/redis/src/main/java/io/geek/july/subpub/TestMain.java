2f1fad9f196fb2541e59119e9be0aca21d3e417e
July <aixiaohui@126.com>
2021-01-03 22:51:06 +0800

    基于 Redis 的 PubSub 实现订单异步处理

---
+++ Week_11/redis/src/main/java/io/geek/july/subpub/TestMain.java
@@ -0,0 +1,13 @@
+package io.geek.july.subpub;
+
+import redis.clients.jedis.JedisPool;
+
+public class TestMain {
+    public static void main(String[] args) {
+        JedisPool jedisPool = new JedisPool();
+        String channelName = "ORDER";
+
+        SubscribeOrder subscribeOrder = new SubscribeOrder(jedisPool, channelName);
+        PublishOrder publishOrder = new PublishOrder(jedisPool, channelName);
+    }
+}
